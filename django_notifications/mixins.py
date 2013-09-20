8b302c54510233cbb1815abc7f25ec46c7d7cf65
Troy Grosfield <troy.grosfield@gmail.com>
2013-09-20 08:09:56 -0600

    added doc, notification source filter, pep8 fixes

--- django_notifications/mixins.py
+++ django_notifications/mixins.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
-from .models import Notification
 from django.core.paginator import EmptyPage
 from django.core.paginator import Paginator
+
+from .constants import NotificationSource
+from .models import Notification
 
 
 class NotificationViewMixin(object):
     """Notifications view mixin that returns the notification_obj's paginator
     and current page.
+
+    Filtering
+    =========
+    You can further filter the notifications by passing the following query
+    string params:
+
+    * ns: notification source.  Can be one of .contants.NotificationSource.
 
     Note: This mixin requires the django_core.mixins.paging.PagingViewMixin
     to be called before this view is called.
@@ -16,10 +25,19 @@ class NotificationViewMixin(object):
 
     def get_context_data(self, **kwargs):
         context = super(NotificationViewMixin, self).get_context_data(**kwargs)
-        # TODO: Might want to prefetch all the notification replies as well
+
+        notification_kwargs = {'obj': self.notifications_about_object}
+        notification_source = NotificationSource.check(
+                                                    self.request.GET.get('ns'))
+
+        if notification_source:
+            notification_kwargs['source'] = notification_source
+
         notifications = (Notification.objects
-                           .get_for_object(obj=self.notifications_about_object)
-                           .prefetch_related('about'))
+                           .get_for_object(**notification_kwargs)
+                           .prefetch_related('about',
+                                             'replies',
+                                             'created_user'))
 
         paginator = Paginator(notifications, self.page_size)
         context['notifications_paginator'] = paginator
@@ -29,4 +47,5 @@ class NotificationViewMixin(object):
         except EmptyPage:
             context['notifications_page'] = paginator.page(paginator.num_pages)
 
+        context['notifications_about_object'] = self.notifications_about_object
         return context
