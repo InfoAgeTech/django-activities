b913a9a88be9d798c314c1c8707bb9e1498d6950
Troy Grosfield <troy.grosfield@gmail.com>
2014-01-14 09:19:41 -0700

    setup fix

--- setup.py
+++ setup.py
@@ -31,18 +31,12 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'https://github.com/InfoAgeTech/django-generic/tarball/master',
-        'https://github.com/InfoAgeTech/django-core/tarball/master',
-        'https://github.com/InfoAgeTech/python-tools/tarball/master'
-    ],
     setup_requires=[
         'django >= 1.5.5',
     ],
     test_suite='nose.collector',
     tests_require=[
-        'django_nose',
-        'https://github.com/InfoAgeTech/django-testing/tarball/master'
+        'django_nose'
     ],
     classifiers=classifiers
 )
