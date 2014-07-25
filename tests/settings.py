4f1a9b31a238b043cec1f1825fa5b9ad4b3b00aa
Troy Grosfield <troy.grosfield@gmail.com>
2014-07-24 22:31:19 -0600

    removed the print statements and cleaned up unnecessary code.

--- tests/settings.py
+++ tests/settings.py
@@ -8,7 +8,7 @@ NOTIFICATION_MODEL_MIXIN = 'test_models.AbstractNotificationMixin'
 NOTIFICATION_MANAGER = 'test_models.managers.NotificationManager'
 NOTIFICATIONS_BASE_TEMPLATE = 'base_notifications.html'
 
-DEBUG = True
+DEBUG = False
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -54,16 +54,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 SITE_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../')
-print(SITE_ROOT)
 TEMPLATE_DIRS = (
-    'templates',
     os.path.join(SITE_ROOT, 'django_notifications/templates')
-)
-print(TEMPLATE_DIRS)
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader'
 )
 
 ROOT_URLCONF = 'urls'
