a543a4764d8a2bbcd2728569b7e04e46c24c9cfb
Troy Grosfield <troy.grosfield@gmail.com>
2014-08-12 12:48:05 -0600

    code cleanup

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,43 +1,22 @@
 from __future__ import unicode_literals
 
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django_testing.user_utils import create_user
-
 from activities.constants import Action
 from activities.constants import Privacy
 from activities.constants import Source
-from activities.models import Activity
 from activities.mixins.views import ActivityViewMixin
-from mock import MagicMock
+from activities.models import Activity
+from django.contrib.auth import get_user_model
 from django.http.response import Http404
+from django.test import TestCase
+from django_testing.user_utils import create_user
+from mock import MagicMock
 
 
 User = get_user_model()
 
 
-class ActivityManagerTests(TestCase):
-    """
-    TODO: could break this up into smaller test cases that would test each
-    individual section. Example:
-        1. test for creating recurring bill
-        2. test for verifying recurring bill activities exist
-        3. test for update recurring bill
-        4. test for deleting bill.
-    """
-
-#     @classmethod
-#     def setUpClass(cls):
-#         """Run once per test case"""
-#         # TODO:
-#         #    1. add x number of users self.user_1, self.user_2...
-#         super(ActivityManagerTests, cls).setUpClass()
-#         cls.user = create_user()
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         super(ActivityManagerTests, cls).tearDownClass()
-#         cls.user.delete()
+class ActivityViewMixinTests(TestCase):
+    """Testcase for activities view mixins."""
 
     def test_activity_view_mixin_success(self):
         """Test ActivityViewMixin successfully returns the public activity."""
@@ -113,6 +92,7 @@ class ActivityManagerTests(TestCase):
 
         mixin = ActivityViewMixin()
         mixin.request = MagicMock(user=user_3)
+
         with self.assertRaises(Http404):
             mixin.get_activity(**{
                 mixin.activity_pk_url_kwarg: activity.id
