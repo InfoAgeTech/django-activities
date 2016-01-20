c1474dba85ece001aab1db1216855731b37fccb2
Troy Grosfield <troy.grosfield@gmail.com>
2016-01-20 14:11:53 -0700

    Updated for django 1.9 and python 3.5

--- activities/__init__.py
+++ activities/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.apps.config import AppConfig
 from django.core.exceptions import ImproperlyConfigured
 
 
@@ -10,7 +11,6 @@ def get_activity_model():
     allow you to set the model instance to a different model subclass.
     """
     from django.conf import settings
-    from django.db.models import get_model
 
     if not hasattr(settings, 'ACTIVITY_MODEL'):
         from .models import Activity
@@ -22,7 +22,7 @@ def get_activity_model():
         raise ImproperlyConfigured("ACTIVITY_MODEL must be of the form "
                                    "'app_label.model_name'")
 
-    activity_model = get_model(app_label, model_name)
+    activity_model = AppConfig.get_model(app_label, model_name)
 
     if activity_model is None:
         raise ImproperlyConfigured("ACTIVITY_MODEL refers to model '%s' "
